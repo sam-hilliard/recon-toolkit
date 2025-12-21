ed6197f7c7bd22fac6a0e5882a515bc50058acc7
Sam Hilliard <samhilliard51@gmail.com>
2025-12-20 19:06:58 -0500

    created basic recon script

--- recon.sh
+++ recon.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-echo "hi"
+subfinder -silent | anew subdomains.txt
+
+cat subdomains.txt | shuffledns -w /opt/wordlists/commonspeak2.txt -r resolvers.txt | anew resolved_subdomains.txt
+
+cat resolved_subdomains.txt | naabu -p 22,21,25,80,443,8080,3000,139,445,3306,5432,6379,54321,5000 | anew open_ports.txt
+
+cat resolved_subdomains.txt | httpx -sc -cl -ct -title -server -silent -threads 20 | anew httpx_results.txt
